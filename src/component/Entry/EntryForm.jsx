28800b7e44f57819ac81d3dd1fde69a4447f763d
Amarnadh <cpamarnadh843@gmail.com>
2024-01-29 15:07:19 +0530

    feat(select ant to mui):updated the cash or credit seleciton to mui component

--- src/component/Entry/EntryForm.jsx
+++ src/component/Entry/EntryForm.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import PlusIcon from "../../assests/images/AddNew.svg";
 import SupplierCreation from "./SupplierCreation";
 import NavigationBar from "../Navbar/NavigationBar";
-import { Select, AutoComplete, Spin } from "antd";
+import { AutoComplete, Spin } from "antd";
 import { fetchDataFromApi } from "../../services/Api";
-
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 function EntryForm() {
+  const [mode, setMode] = useState("dar");
+
   // modal opeing state management
   const [isModalOpen, setIsModalOpen] = useState(false);
   const toggleModal = () => {
@@ -24,6 +27,8 @@ function EntryForm() {
       timeout = setTimeout(() => func(...args), delay);
     };
   };
+
+  const [paymentType, setPayment] = useState(10);
 
   // Api calling with Debouncing
   const getPanelValue = debounce(async (searchText) => {
@@ -43,12 +48,12 @@ function EntryForm() {
       setLoading(false);
     }
   }, 300);
-  
-  //form submition function 
-  const handleSubmit=(e)=>{
+
+  //form submition function
+  const handleSubmit = (e) => {
     e.preventDefault();
-  console.log("save");
-  }
+    console.log("save");
+  };
 
   //for unmounting the sate
   useEffect(() => {
@@ -58,11 +63,13 @@ function EntryForm() {
   return (
     <div className="my-0 mx-auto flex flex-col items-end text-xs md:text-base overflow-auto mt-0 ">
       <NavigationBar />
-      <form onSubmit={handleSubmit} className="transaction-form w-full text-md md:text-base font-inter">
+      <form
+        onSubmit={handleSubmit}
+        className="transaction-form w-full text-md md:text-base font-inter"
+      >
         <div className="flex items-center justify-between mb-4">
           <label className="flex gap-2 w-1/4 text-left">Supplier</label>
           <div className="flex justify-between w-3/4">
-            
             <AutoComplete
               options={options}
               type={"primary"}
@@ -86,15 +93,15 @@ function EntryForm() {
         </div>
 
         <div className="flex items-center mb-4">
-          <label htmlFor="date" className="inline-block w-1/4 text-left">
+          <label htmlFor="date" className="inline-block w-1/4 text-left ">
             Date
           </label>
           <input
             type="date"
             id="date"
             name="date"
-            defaultValue="2023-08-09"
-            className="w-3/4 p-2 cursor-text rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700 cursor: pointer;"
+            defaultValue={new Date().toISOString().split("T")[0]}
+            className="w-3/4 p-2 cursor-text rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700 cursor: pointer; "
           />
         </div>
 
@@ -110,7 +117,7 @@ function EntryForm() {
             id="invoice-number"
             name="invoice-number"
             defaultValue="26855285278"
-            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700"
+            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700 font-inter"
           />
         </div>
 
@@ -123,7 +130,7 @@ function EntryForm() {
             id="amount"
             name="amount"
             defaultValue="1000"
-            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700"
+            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700 font-inter"
           />
         </div>
 
@@ -136,28 +143,69 @@ function EntryForm() {
             id="vat"
             name="vat"
             defaultValue="100"
-            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700"
+            className="w-3/4 p-2 rounded-lg bg-inputColor border focus:outline-none focus:border-purple-700 font-inter"
           />
         </div>
 
         <div className="flex items-center">
           <label className="inline-block w-1/4 text-left">Cash / Credit</label>
           <Select
-            defaultValue="Cash"
-            size={"large"}
-            className="w-3/4   rounded-lg bg-red   focus:border-purple-700"
-            onChange={(value) => console.log("Selected - ", value)}
-            options={[
-              { value: "Cash", label: "Cash" },
-              { value: "Credit", label: "Credit" },
-            ]}
-          />
+            value={paymentType}
+            onChange={(event) => setPayment(event.target.value)}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            sx={{
+              width: "77%",
+              height: "40px",
+              backgroundColor: mode === "dark" ? "#232323" : "#E9E9E9",
+              color: mode === "dark" ? "white" : "black",
+              borderRadius: "8px",
+              border: "none",
+              outline: "none",
+              "&:hover": {
+                outline: "none",
+                    border: "1px solid purple", 
+              },
+            }}
+            MenuProps={{
+              sx: {
+                "& .MuiMenu-paper": {
+                  backgroundColor: mode === "dark" ? "#232323" : "#E9E9E9",
+                },
+              },
+            }}
+          >
+            <MenuItem
+              value={10}
+              sx={{
+                backgroundColor: mode === "dark" ? "#232323" : "#E9E9E9",
+                color: mode === "dark" ? "white" : "black",
+                "&:hover": {
+                  backgroundColor: mode === "dark" ? "#121212" : "#CCCCCC", 
+                },
+              }}
+            >
+              cash
+            </MenuItem>
+            <MenuItem
+              value={20}
+              sx={{
+                backgroundColor: mode === "dark" ? "#232323" : "#E9E9E9",
+                color: mode === "dark" ? "white" : "black",
+                "&:hover": {
+                  backgroundColor: mode === "dark" ? "#121212" : "#CCCCCC", 
+                },
+              }}
+            >
+              credit
+            </MenuItem>
+          </Select>
         </div>
 
         <div className="flex justify-end items-center my-2">
           <label
             htmlFor="photo-upload"
-            className="text-purple-600 text-right text-base font-inter underline cursor-pointer"
+            className="text-purple-600 text-right  font-inter underline cursor-pointer text-sm "
           >
             Upload Photo
           </label>
