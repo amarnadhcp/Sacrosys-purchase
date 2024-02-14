1d31bbb43864e231af4f99a4928f27aa228bccd0
Amarnadh <cpamarnadh843@gmail.com>
2024-02-14 13:04:00 +0530

    fix(entry table border)

--- src/component/Entry/EntryTable.jsx
+++ src/component/Entry/EntryTable.jsx
@@ -36,18 +36,18 @@ function EntryTable() {
             <RangePicker onChange={handleDateRangeChange} />
           </div>
           <div className="overflow-y-auto max-h-96">
-          <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400 z-0">
-            <thead className="sticky top-0 text-xs text-white font-inter bg-custom-black text-center ">
+          <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400 z-0 border-collapse">
+            <thead className="sticky z-30 top-0 text-xs text-white font-inter bg-custom-black text-center ">
               <tr>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Date</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Supplier</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Invoice Number</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Amount</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">VAT</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Cash/Credit</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Image</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">Action</th>
-                <th className="px-2 py-2 md:px-4 md:py-4 border-b border-r border-solid border-white/20">button</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Date</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Supplier</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Invoice Number</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Amount</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">VAT</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Cash/Credit</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Image</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">Action</th>
+                <th className="px-2 py-2 md:px-4 md:py-4 border-b-2 border-r-2 border-solid border-[#3A3A3A]">button</th>
               </tr>
             </thead>
             <tbody>
